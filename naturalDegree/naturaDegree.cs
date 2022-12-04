00ad3ccfba3185d11b285d5c479beea49f709b6e
Kostya Vasilyev <kostiavasilev10@gamil.com>
2022-12-04 18:45:23 +0300

    add naturalDegree

---
+++ naturalDegree/naturaDegree.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int naturelDegree(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+int NaturalDegree = naturelDegree(numberA, numberB);
+Console.WriteLine("Ответ: " + NaturalDegree);
+
